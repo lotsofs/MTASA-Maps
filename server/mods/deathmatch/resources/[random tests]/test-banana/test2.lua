06333af1ae3b3ffeb4c403e80f38ff11abcd35a6
lotsofs <oeneboy@hotmail.com>
2020-03-23 20:09:53 +0100

    [Random tests]

---
+++ server/mods/deathmatch/resources/[random tests]/test-banana/test2.lua
@@ -0,0 +1,24 @@
+-- car = nil
+
+-- function aaa(thePlayer, seat, jacked)
+	-- if (getElementType(thePlayer) ~= "player") then
+		-- return
+	-- end
+	-- -- asdf = getModelHandling(476)
+	-- -- for k,g in pairs(asdf) do
+		-- -- setVehicleHandling(source, k, g)
+	-- -- end
+	-- local sir = createPed(120,0,0,0)
+	-- if (car) then
+		-- destroyElement(car)
+	-- end
+	-- car = createVehicle(476,0,0,0)
+	-- warpPedIntoVehicle(sir, car)
+	-- attachElements(car,source,0,0,1,13,0,0)
+		-- setElementAlpha(thePlayer, 0)
+		-- setTimer(function(source)
+		-- setElementAlpha(source, 0)
+
+	-- end, 1000, 10, source)
+-- end
+-- addEventHandler("onVehicleEnter", root, aaa)
---
+++ server/mods/deathmatch/resources/[random tests]/test-rustler/test.lua
@@ -0,0 +1,38 @@
+function aaa()
+	txd = engineLoadTXD("rustler.txd")
+	engineImportTXD(txd, 555)
+	dff = engineLoadDFF("rustler.dff")
+	engineReplaceModel(dff, 555)
+end
+addCommandHandler("replace", aaa)
+--addEventHandler("onClientResourceStart", resourceRoot, aaa)
+
+
+
+-- car = nil
+-- sir = nil
+
+-- function aaa()
+	-- -- asdf = getModelHandling(476)
+	-- -- for i, v in pairs(asdf) do
+		-- -- setModelHandling(476, i, v)
+	-- -- end
+	-- -- for k,_ in pairs(getModelHandling(476)) do
+		-- -- setModelHandling(476, k, nil)
+	-- -- end
+	-- vehicle = getPedOccupiedVehicle(localPlayer)
+	-- x,y,z = getElementPosition(vehicle)
+	-- if (car) then
+		-- destroyElement(car)
+	-- end
+	-- sir = createPed(120,x+10,y,z)
+	-- car = createVehicle(476,x+12, y, z)
+	-- warpPedIntoVehicle(sir, car)
+	-- attachElements(car,vehicle,0,0,1,10,0,0)
+	-- setElementAlpha(localPlayer, 0)
+	-- setTimer(function(vehicle)
+		-- setElementAlpha(vehicle, 0)
+	-- end, 1000, 10, vehicle)
+	-- setPedControlState(sir, "accelerate", true)
+-- end
+-- addCommandHandler("a", aaa)
