c07c96efe73059685e73b96c725ec641a5245b29
lotsofs <oeneboy@hotmail.com>
2020-03-31 00:30:29 +0200

    [555 We Tip] V1.0

--- server/mods/deathmatch/resources/wip-555wetip/555_client.lua
+++ server/mods/deathmatch/resources/wip-555wetip/555_client.lua
@@ -1,3 +1,9 @@
+DA = nil
+BLIP = nil
+BLIP2 = nil
+TEXT = nil
+TIMER = nil
+
 function playDACutscene()
 	-- show the DA driving to the carport
 		--  <vehicle id="Cutscene_Car" sirens="false" paintjob="3" interior="0" alpha="255" model="551" plate="M0BOP47" dimension="0" color="88,88,83,245,245,245,0,0,0,0,0,0" 
@@ -6,15 +12,23 @@ function playDACutscene()
 	setVehicleColor(car, 4, 4, 4, 4)
 		--  <ped id="DA" dimension="0" model="240" interior="0" rotZ="0.003" frozen="false" alpha="255" posX="-1732.4" posY="986.29999" posZ="83" rotX="0" rotY="0"></ped>
 	ped = createPed(240, -1696, 919.2, 24.6)
+	DA = ped
 	warpPedIntoVehicle(ped, car)
 	setPedControlState(ped, "accelerate", true)
 	setCameraMatrix(-1759.9, 962.8, 26.0, -1755.5, 943.5, 25)
 	setTimer(function()
 		setPedControlState(ped, "accelerate", false)
 		setPedControlState(ped, "handbrake", true)
+		playSound("tipper.wav")
 	end, 3050, 1)
 	setTimer(function()
-		-- remove DA from vehicle, remove vehicle, place DA next to vehicle, have him walk away
+		playerX, playerY, playerZ = getElementPosition(localPlayer)
+		if (playerY < 500) then
+			destroyElement(ped)
+			destroyElement(car)
+			return
+		end
+			-- remove DA from vehicle, remove vehicle, place DA next to vehicle, have him walk away
 		setPedControlState(ped, "handbrake", false)
 		removePedFromVehicle(ped)
 		destroyElement(car)
@@ -29,20 +43,100 @@ end
 addEvent("playDACutscene", true)
 addEventHandler("playDACutscene", resourceRoot, playDACutscene)
 
+function prepareHotelGarage()
+	if (DA) then
+		setPedControlState(DA, "forwards", false)
+		setPedControlState(DA, "walk", false)
+			-- <ped id="putDAHere" dimension="0" model="240" interior="0" rotZ="0" alpha="255" 
+			-- posX="-1755.3" posY="947" posZ="24.9" rotX="0" rotY="0"></ped>
+		setElementPosition(DA, -1755.3, 947, 24.9)
+		setElementRotation(DA, 0, 0, 0, "default", true)
+	end
+	marker = getElementByID("marker_undergroundGarageParkingSpot")
+	setElementAlpha(marker, 255)
+	TEXT = "The drugs are planted. \nTake the car to the valet's #FE0000car park."
+	TIMER = setTimer(function()
+		TEXT = "Keep the car spotless or the D.A. will notice."
+		TIMER = setTimer(function()
+			TEXT = nil
+			TIMER = nil
+		end, 5000, 1)
+	end, 5000, 1)
+end
+addEvent("prepareHotelGarage", true)
+addEventHandler("prepareHotelGarage", resourceRoot, prepareHotelGarage)
 
+function promptVehicleRepair(enabled)
+	if (TIMER) then
+		killTimer(TIMER)
+		TIMER = nil
+	end
+	marker = getElementByID("marker_undergroundGarageParkingSpot")
+	marker2 = getElementByID("marker_garageRepair")
+	if (enabled) then
+		setElementAlpha(marker, 0)
+		setElementAlpha(marker2, 255)
+		if (BLIP) then
+			destroyElement(BLIP)
+			BLIP = nil
+		end
+		BLIP = createBlip(-1904.3, 286.89999, 41.6, 63)
+		if (BLIP2) then
+			destroyElement(BLIP2)
+			BLIP2 = nil
+		end
+		BLIP2 = createBlip(-2033.626, 178.85645, 27.8359, 0, 2, 0, 255, 0)
+		-- prompt player to repair their vehicle
+		TEXT = "The car's noticeably damaged. \nTake it back to #00FE00the Garage#FFFFFF to get it fixed up."
+	else
+		setElementAlpha(marker, 255)
+		setElementAlpha(marker2, 0)
+		if (BLIP) then
+			destroyElement(BLIP)
+			BLIP = nil
+		end
+		if (BLIP2) then
+			destroyElement(BLIP2)
+			BLIP2 = nil
+		end
+		TEXT = nil
+	end
+end
+addEvent("promptVehicleRepair", true)
+addEventHandler("promptVehicleRepair", resourceRoot, promptVehicleRepair)
 
+function promptPlayerParkInMarker()
+	if (TEXT) then
+		return
+	end
+	TEXT = "Park the car in the marked #FE0000parking space."
+	TIMER = setTimer(function()
+		TEXT = nil
+		TIMER = nil
+	end, 5000, 1)
+end
+addEvent("promptPlayerParkInMarker", true)
+addEventHandler("promptPlayerParkInMarker", resourceRoot, promptPlayerParkInMarker)
 
+function drawText()
+	if (not TEXT) then
+		return
+	end
+	local width,height = guiGetScreenSize()
+	drawBorderedText(TEXT, 2, width*0.2, height*0.7, width*0.8, height*0.9, tocolor(255, 255, 255, 255), 3, "default", "center", "top", false, true, false, true)
+end
 
+function drawBorderedText(text, borderSize, width, height, width2, height2, color, size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	text2 = string.gsub(text, "#%x%x%x%x%x%x", "")
+	dxDrawText(text2, width+borderSize, height, width2+borderSize, height2, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width, height+borderSize, width2, height2+borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width, height-borderSize, width2, height2-borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width-borderSize, height, width2-borderSize, height2, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width+borderSize, height+borderSize, width2+borderSize, height2+borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width-borderSize, height-borderSize, width2-borderSize, height2-borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width+borderSize, height-borderSize, width2+borderSize, height2-borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text2, width-borderSize, height+borderSize, width2-borderSize, height2+borderSize, tocolor(0, 0, 0, 255), size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+	dxDrawText(text, width, height, width2, height2, color, size, font, horizAlign, vertiAlign, bool1, bool2, bool3, bool4)
+end
 
--- function cutscene(newState, oldState)
-	-- if (newState == "GridCountdown") then
-		-- for i, v in pairs(getElementsByType("player")) do
-			-- setCameraMatrix(v, CAMERA_POSITION_X, CAMERA_POSITION_Y, CAMERA_POSITION_Z, CAMERA_TARGET_X, CAMERA_TARGET_Y, CAMERA_TARGET_Z)
-			-- setTimer(setCameraTarget, 5000, 1, v, v)
-		-- end
-	-- elseif (newState == "Running") then
-		-- for i, v in pairs(getElementsByType("player")) do
-			-- setCameraTarget(v, v)
-		-- end
-	-- end
--- end
+addEventHandler("onClientRender", root, drawText)
--- server/mods/deathmatch/resources/wip-555wetip/555_server.lua
+++ server/mods/deathmatch/resources/wip-555wetip/555_server.lua
@@ -1,7 +1,23 @@
 	-- todo: Play "HEY THATS THE DA. HES A GOOD TIPPER" sound
 	-- todo: rest of the map
+MARKER = getElementByID("marker_undergroundGarageParkingSpot")
+MARKER_REPAIR = getElementByID("marker_garageRepair")
+CHECKPOINT2 = getElementByID("checkpoint2cutscene")
+CHECKPOINT3 = getElementByID("checkpoint3cutscene")
+CHECKPOINT6 = getElementByID("checkpoint6final")
+
+function onMapStarting(mapInfo, mapOptions, gameOptions)
+	setElementAlpha(MARKER, 0)
+	setElementAlpha(MARKER_REPAIR, 0)
+end
+addEventHandler("onMapStarting", root, onMapStarting)
+
+-- race progress
+-- -------------
+-- -------------
 
 function onPlayerReachCheckpoint(checkpoint, time_)
+	name = getPlayerName(source)
 	if (checkpoint == 1) then
 		-- player arrives at hotel
 		-- move player away
@@ -13,50 +29,159 @@ function onPlayerReachCheckpoint(checkpoint, time_)
 		-- play cutscene
 		triggerClientEvent(source, "playDACutscene", resourceRoot)
 		setTimer(function(vehicle, player)
+			x,y,z = getElementPosition(vehicle)
+			if (z < 80) then
+				return
+			end
 				--  <vehicle id="Place_Car_Here_Hotel colors 4 4 0 0" sirens="false" paintjob="3" interior="0" alpha="255" model="551" plate="9IA8P8R" dimension="0" 
-				--  color="88,88,83,245,245,245,0,0,0,0,0,0" posX="-1757.8" posY="953.70001" posZ="24.6" rotX="0" rotY="0" rotZ="90"></vehicle>
+				--  color="88,88,83,245,245,245,0,0,0,0,0,0" posX="-1757.8" posY="953.70001" posZ="25" rotX="0" rotY="0" rotZ="90"></vehicle>
 			-- teleport player into DA car in parking spot
 			setElementFrozen(vehicle, false)
 			toggleAllControls(player, true)
-			checkpoint = getElementByID("checkpoint2cutscene")
-			checkpoint2 = getElementByID("checkpoint3cutscene")
-			setElementPosition(vehicle, getElementPosition(checkpoint))
-			setElementPosition(vehicle, getElementPosition(checkpoint2))
-			setElementPosition(vehicle, -1757.8, 953.7, 24.6)
+			setElementPosition(vehicle, getElementPosition(CHECKPOINT2))
+			setElementPosition(vehicle, getElementPosition(CHECKPOINT3))
+			setElementPosition(vehicle, -1757.8, 953.7, 25)
 			setElementModel(vehicle, 551)
 			setVehicleColor(vehicle, 4, 4, 4, 4)
 			fixVehicle(vehicle)
 			setElementRotation(vehicle, 0, 0, 90)
 		end, 6000, 1, vehicle, source)
+	elseif (checkpoint == 4) then
+		-- player arrives at garage
+		vehicle = getPedOccupiedVehicle(source)
+			-- <vehicle id="Vehicle_ParkMeritHereAfterRepair" sirens="false" paintjob="3" interior="0" alpha="255" model="551" plate="CZ95Z56" 
+			-- dimension="0" color="54,65,85,245,245,245,0,0,0,0,0,0" posX="-2031.5" posY="178.60001" posZ="28.7" rotX="0" rotY="0" rotZ="270"></vehicle>
+		-- replace vehicle
+		setElementPosition(vehicle, -2031.5, 178.6, 28.7)
+		setElementRotation(vehicle, 0, 0, 270)
+		setElementVelocity(vehicle, 0, 0, 0)
+		setElementAngularVelocity(vehicle, 0, 0, 0)
+		fixVehicle(vehicle)
+		-- move DA
+		triggerClientEvent(source, "prepareHotelGarage", resourceRoot)
+	elseif (checkpoint == 5) then
+		triggerClientEvent(source, "promptPlayerParkInMarker", resourceRoot)
 	end
 end
 addEventHandler("onPlayerReachCheckpoint", root, onPlayerReachCheckpoint)
 
--- player reaches third checkpoint
--- repair the car
--- place car in right orientation
--- despawn the DA
+function playerStoppedInMarker()
+	-- player is in the marker at the parking spot inside the garage
+	for i,v in ipairs(getElementsByType("player")) do	
+		currentCheckpoint = getElementData(v, "checkpoint")
+		if (currentCheckpoint == "5/6") then
+			vehicle = getPedOccupiedVehicle(v)
+			if (vehicle and isElementWithinMarker(vehicle, MARKER)) then
+				health = getElementHealth(vehicle)
+				if (health == 1000) then
+					velocity = getDistanceBetweenPoints3D(0,0,0,getElementVelocity(vehicle))
+					if (velocity < 0.001) then
+						x,y,z = getElementPosition(vehicle)
+						setElementPosition(vehicle, getElementPosition(CHECKPOINT6))
+						setElementPosition(vehicle, x,y,z)
+					end
+				end
+			end
+		end
+	end
+end
+setTimer(playerStoppedInMarker, 1, 0)
 
--- if car is damaged
--- disable the final marker
--- prompt player to repair their vehicle
--- place a blip at pay n spray
--- place a blip at the garage
 
--- player goes to garage after already having been there
--- repair the car
--- place car in right orientation
+-- damage stuff
+-- ------------
+-- ------------
 
--- player reaches fourth checkpoint
--- prompt player to park in the marker
+function onVehicleDamage(loss)
+	-- vehicle damaged
+	player = getVehicleOccupant(source)
+	if (not getElementType(player) == "player") then
+		return
+	end
+	currentCheckpoint = getElementData(player, "checkpoint")
+	if (currentCheckpoint == "4/6" or currentCheckpoint == "5/6") then
+		-- merit is damaged on the way back to the hotel, prompt for repair
+		triggerClientEvent(player, "promptVehicleRepair", resourceRoot, true)
+	end
+end
+addEventHandler("onVehicleDamage", root, onVehicleDamage)
 
--- player parks in marker
--- teleport player to last checkpoint
--- teleport player back
+function onPlayerWasted()
+	triggerClientEvent(source, "promptVehicleRepair", resourceRoot, false)
+end
+addEventHandler("onPlayerWasted", root, onPlayerWasted)
 
--- player dies
--- if at start = nothing needed to be done
--- if during cutscene = WHY?
--- if after cutscene = check if they respawn on top of the building, then teleport?, this might not be completely foolrpoof though
--- if after garage = idk
+function onPlayerRepairMarkerHit(hitElement, matchingDimension)
+	if (getElementType(hitElement) ~= "player") then
+		return
+	end
+	currentCheckpoint = getElementData(hitElement, "checkpoint")
+	if (currentCheckpoint ~= "4/6" and currentCheckpoint ~= "5/6") then
+		return
+	end
+	vehicle = getPedOccupiedVehicle(hitElement)
+	health = getElementHealth(vehicle)
+	if (health < 1000) then
+		fixVehicle(vehicle)
+		setElementPosition(vehicle, -2031.5, 178.6, 28.7)
+		setElementRotation(vehicle, 0, 0, 270)
+		setElementVelocity(vehicle, 0, 0, 0)
+		setElementAngularVelocity(vehicle, 0, 0, 0)
+		triggerClientEvent(hitElement, "promptVehicleRepair", resourceRoot, false)	
+	end
+end
+addEventHandler("onMarkerHit", MARKER_REPAIR, onPlayerRepairMarkerHit)
+
+function onPlayerPickUpRacePickup(pickupID, pickupType, vehicleModel)
+	if (pickupType ~= "repair") then
+		return
+	end
+	if (currentCheckpoint ~= "4/6" and currentCheckpoint ~= "5/6") then
+		return
+	end
+	triggerClientEvent(player, "promptVehicleRepair", resourceRoot, false)	
+end
+addEvent("onPlayerPickUpRacePickup", true)
+addEventHandler("onPlayerPickUpRacePickup", root, onPlayerPickUpRacePickup)
+
+function processRespawn(vehicle)
+	player = getVehicleOccupant(vehicle)
+	if (not player) then
+		return
+	end
+	checkpoint = getElementData(player, "checkpoint")
+	if (checkpoint == "0/6" or checkpoint == false) then
+		return
+	elseif (checkpoint == "1/6" or checkpoint == "2/6" or checkpoint == "3/6") then
+		-- player respawned at the hotel
+		setElementPosition(vehicle, getElementPosition(CHECKPOINT2))
+		setElementPosition(vehicle, getElementPosition(CHECKPOINT3))
+		setElementModel(vehicle, 551)
+		setVehicleColor(vehicle, 4, 4, 4, 4)
+		setElementRotation(vehicle, 0, 0, 90)
+		setElementPosition(vehicle, -1757.8, 953.7, 25)	
+		setElementVelocity(vehicle, 0, 0, 0)
+		setElementAngularVelocity(vehicle, 0, 0, 0)
+	elseif (checkpoint == "4/6") then
+		-- player respawned at the garage
+			--setElementPosition(vehicle, -2031.5, 178.6, 28.7)
+			--setElementRotation(vehicle, 0, 0, 270)
+		setVehicleColor(vehicle, 4, 4, 4, 4)
+		setElementRotation(vehicle, 0, 0, 90)
+		setElementVelocity(vehicle, 0, 0, 0)
+		setElementAngularVelocity(vehicle, 0, 0, 0)
+		setElementPosition(vehicle, -1757.8, 953.7, 25)	
+		setElementHealth(vehicle, 400)
+	elseif (checkpoint == "5/6") then
+		setVehicleColor(vehicle, 4, 4, 4, 4)
+		setElementHealth(vehicle, 400)
+	end
+end
+
+function onVehicleEnter(thePlayer, seat, jacked)
+	name = getPlayerName(thePlayer)
+	setTimer(processRespawn, 2000, 1, source)
+	-- player is respawning
+end
+addEventHandler("onVehicleEnter", root, onVehicleEnter)
 
