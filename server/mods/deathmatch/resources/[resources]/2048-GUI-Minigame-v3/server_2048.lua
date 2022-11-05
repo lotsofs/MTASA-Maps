23804fa4f751b09105b5b52e65b74198edabec8b
lotsofs <oeneboy@hotmail.com>
2022-11-05 02:00:18 +0100

    Remove dead meme minigame

--- server/mods/deathmatch/resources/[resources]/2048-GUI-Minigame-v3/server_2048.lua
+++
@@ -1,9 +0,0 @@
--- 2048 in MTA by Ali Digitali
--- the original game can be found at: http://gabrielecirulli.github.io/2048/
--- anyone reading this has permission to copy parts of this script
-
-function winMessage ()
-    outputChatBox (getPlayerName(client).."#00FF00 has reached 2048!", getRootElement(),0,255,0,true)
-end
-addEvent( "on2048win", true )
-addEventHandler( "on2048win", root, winMessage )
